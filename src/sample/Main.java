07023527f219595492744fe64313a92734ecd1c4
twittwer9029 <twittwer9029@eagle.fgcu.edu>
2018-09-10 08:02:01 -0400

    Java FX Tutorial 2 Adding the following files to the src folder: Controller.java sample.fxml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -23,7 +23,7 @@ public class Main extends Application {
         grid.setVgap(10);
         grid.setPadding(new Insets(25,25,25,25));
 
-        Text scenetitle = new Text("Welcome");
+        Text scenetitle = new Text("Welcome!");
         scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
         grid.add(scenetitle, 0,0,2,1);
 
