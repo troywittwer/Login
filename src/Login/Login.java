638471adfbdaf4ee9870d005b7d5a5c87b9b1646
twittwer9029 <twittwer9029@eagle.fgcu.edu>
2018-09-14 09:02:24 -0400

    Java FX Tutorial 3 Added the CSS style sheet and refactored files to reflect the "Login.java" file name format

--- src/Login/Login.java
+++ src/Login/Login.java
@@ -1,8 +1,7 @@
-package sample;
+package Login;
 
 import javafx.event.*;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
@@ -11,7 +10,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.*;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Login extends Application {
 
     @Override
     public void start(Stage primaryStage){
@@ -23,8 +22,8 @@ public class Main extends Application {
         grid.setVgap(10);
         grid.setPadding(new Insets(25,25,25,25));
 
-        Text scenetitle = new Text("Welcome!");
-        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setId("welcome-text");
         grid.add(scenetitle, 0,0,2,1);
 
         Label userName = new Label("User Name:");
@@ -46,18 +45,20 @@ public class Main extends Application {
         grid.add(hbBtn,1,4);
 
         final Text actiontarget = new Text();
+        actiontarget.setId("actiontarget");
         grid.add(actiontarget,1,6);
 
         btn.setOnAction(new EventHandler<ActionEvent>(){
             @Override
             public void handle(ActionEvent e){
-                actiontarget.setFill(Color.FIREBRICK);
                 actiontarget.setText("Sign in button pressed");
             }
         });
 
         Scene scene = new Scene(grid,300,275);
         primaryStage.setScene(scene);
+        scene.getStylesheets().add
+            (Login.class.getResource("Login.css").toExternalForm());
         primaryStage.show();
     }
 
